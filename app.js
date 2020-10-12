eff04db80da0fbb07cda0e74a193176f1a1c3fcb
MarigonaN <marigonanuhija@gmail.com>
2020-10-12 02:09:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,19 +8,7 @@ app.use(express.json())
 mongoose.connect('mongodb://localhost:27017/e-Medicine', {useNewUrlParser : true}, ()=>{
     console.log('successfully connected to databasee')
 })
-const User = require('./models/User')
 
-const userInput = {
-    username: "Marigona",
-    password: "1234567",
-    role: "admin"
-}
-const user = new User(userInput);
-user.save((err, document)=>{
-    if(err)
-    console.log(err);
-    console.log(document);
-})
 app.listen(3000, ()=>{
     console.log('express server started')
 })
