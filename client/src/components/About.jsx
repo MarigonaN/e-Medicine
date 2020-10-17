45f8d921ff73f4593629ac1302e7f3242877d304
MarigonaN <marigonanuhija@gmail.com>
2020-10-17 21:44:44 +0200

    Update About.jsx

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,52 +2,48 @@ import React, { Component } from 'react';
 class About extends Component {
     render() {
         return (
-           <>
+            <>
 
-            <div className="mt-5">
-                <div className="text-center">
-                    <h2 className=" text-uppercase">About</h2>
-                    <h3 className=" text-muted">About us</h3>
+                <div className="mt-5">
+                    <div className="text-center">
+                        <h2 className=" text-uppercase">About</h2>
+                        <h3 className=" text-muted">About us</h3>
+                    </div>
+                    <ul className=" timeline mt-5">
+                        <li >
+                            <div className="timeline-image"><img className="rounded-circle img-fluid" src="" alt="" /></div>
+                            <div className=" timeline-panel vl">
+
+                                <div>
+
+                                    <h4>July 2020</h4>
+                                    <h4 >Transition to Full Service</h4>
+                                </div>
+
+                                <div ><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                            </div>
+                        </li>
+                        <li className="vl3">
+
+                        </li>
+                        <li className="timeline-inverted">
+                            <div className="timeline-image"><img className="rounded-circle img-fluid" src="" alt="" /></div>
+                            <div className="timeline-panel vl2">
+                                <div>
+                                    <h4>October 2020</h4>
+                                    <h4 >Phase Two Expansion</h4>
+                                </div>
+                                <div><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                            </div>
+                        </li>
+
+
+                    </ul>
                 </div>
-                <ul className=" timeline mt-5">
-                   
-                 
-                    <li >
-                        <div className="timeline-image"><img className="rounded-circle img-fluid" src="" alt="" /></div>
-                     
-                        <div className=" timeline-panel vl">
-                        
-                            <div>
-                          
-                                <h4>July 2020</h4>
-                                <h4 >Transition to Full Service</h4>
-                            </div>
-                            
-                            <div ><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
-                            
-                        </div>
-                    </li>
-                    <li className="vl3">
-                    
-                    </li>
-                    <li className="timeline-inverted">
-                        <div className="timeline-image"><img className="rounded-circle img-fluid" src="" alt="" /></div>
-                        <div className="timeline-panel vl2">
-                            <div>
-                                <h4>October 2020</h4>
-                                <h4 >Phase Two Expansion</h4>
-                            </div>
-                            <div><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
-                        </div>
-                    </li>
-                    
-                    
-                </ul>
-            </div>
-     
-               
-                
-           </>
+
+
+
+            </>
         );
     }
 }
