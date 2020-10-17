d5b53d9220e68ba0b132329339918e8470dafef5
MarigonaN <marigonanuhija@gmail.com>
2020-10-17 21:44:57 +0200

    Update User.js

--- routes/User.js
+++ routes/User.js
@@ -83,8 +83,8 @@ userRouter.get('/admin', passport.authenticate('jwt', { session: false }), (req,
 });
 
 userRouter.get('/authenticated', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const {username, role} = req.user;
-  res.status(200).json({isAuthenticated: true, user: {username, role}});
+    const { username, role } = req.user;
+    res.status(200).json({ isAuthenticated: true, user: { username, role } });
 });
 
 
