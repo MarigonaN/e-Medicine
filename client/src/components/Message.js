b766c09da01ed0daebb4e1d13d871d99a8c15638
MarigonaN <marigonanuhija@gmail.com>
2020-10-17 21:44:50 +0200

    Update Message.js

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const getStyle = (props) =>{
+const getStyle = (props) => {
     let baseClass = "alert";
-    if(props.message.msgError)
-    baseClass = baseClass + "alert-danger";
-    else 
-    baseClass = baseClass + "alert-primary";
+    if (props.message.msgError)
+        baseClass = baseClass + "alert-danger";
+    else
+        baseClass = baseClass + "alert-primary";
     return baseClass + " text-center";
 }
 
 const Message = props => {
-    return(
+    return (
         <div className={getStyle(props)} role="alert">
             {props.message.msgBody}
         </div>
