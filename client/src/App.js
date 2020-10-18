acc3b6f3332947991b7a23944e562f07412bd496
MarigonaN <marigonanuhija@gmail.com>
2020-10-19 01:50:38 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar'
 import Login from './components/Login'
 import Register from './components/Register'
+import Todos from "./components/Todos"
 import './App.css';
 import Home from "./pages/Home"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/todos" component={Todos}/>
       </Router>
     </div>
 
