bafd3fbca8e0ec0b437c21b0e45a877a2a168be8
MarigonaN <marigonanuhija@gmail.com>
2020-10-19 01:04:10 +0200

    Update Todos.js

--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -2,3 +2,13 @@ import React, {useState, useContext, useEffect} from 'react'
 import TodoItem from './TodoItem'
 import TodoService from '../services/TodoServices';
 import {AuthContext} from '../context/AuthContext'
+
+const Todos = props =>{
+    const [todo, setTodo] = useState({name: ""})
+    const [todo, setTodo] = useState([]);
+    const [message, setMessage] = useState(null);
+    const authContext = useContext(AuthContext);
+
+}
+
+export default Todos
