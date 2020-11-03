b3182f8e0cb160427b94e3baab2a0789ee24fff3
MarigonaN <marigonanuhija@gmail.com>
2020-11-03 19:03:18 +0100

    Update PrivateRoute.js

--- client/src/hocs/PrivateRoute.js
+++ client/src/hocs/PrivateRoute.js
@@ -8,7 +8,7 @@ const PrivateRoute = ({component: Component, roles, ...rest})=>{
         <Route {...rest} render={props=> {
             if(!isAuthenticated)
                 return <Redirect to={{pathname: '/login', state: {from: props.location}}}/>
-                if(!roles.included(user.role))
+                if(!roles.includes(user.role))
                     return <Redirect to={{pathname: '/',
                                      state : {from : props.location}}}/>
                                      return <Component {...props}/>
