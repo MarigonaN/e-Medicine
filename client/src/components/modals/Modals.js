7e941badbe8d293e6da293f2ea488a2635002036
MarigonaN <marigonanuhija@gmail.com>
2020-11-08 00:56:29 +0100

    commit

--- client/src/components/modals/Modals.js
+++ client/src/components/modals/Modals.js
@@ -8,7 +8,7 @@ function Modals() {
 
     const closeModalHandler = () => setShow(false);
     return (
-        <div className="container">
+        <div >
             {show ? <div onClick={closeModalHandler} className="back-drop"></div> : null}
             <img  src="https://todaymiddleware.mims.com/resource/image/2C93CB3B-9617-4733-81D4-A68001198DD0/OriginalThumbnail/THUMBNAIL_Fotolia_59344290_Subscription_Monthly_M.jpg" onClick={() => setShow(true)} className="btn-openModal"  ></img>
             
