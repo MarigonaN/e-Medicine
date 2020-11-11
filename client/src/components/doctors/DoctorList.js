3f125f3079e6cafbb7b3cf83b3641a6a95f18a7a
MarigonaN <marigonanuhija@gmail.com>
2020-11-11 23:27:54 +0100

    commit

--- client/src/components/doctors/DoctorList.js
+++ client/src/components/doctors/DoctorList.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import DoctorData from './doctors.json'
+import {Row, Col} from 'react-bootstrap'
 class DoctorList extends Component {
     render() {
         return (
             <div>
                 <h3>Doctors</h3>
+                <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3">
                 {DoctorData.map((doctorDetail, index)=>{
-                    return <img src={doctorDetail.picture} style={{width: "250px", height: "250px", marginBottom: "100px", marginLeft: "100px"}}></img>
+                 
+                 return (
+                    <Col >
+    <img src={doctorDetail.picture} style={{width: "250px", height: "250px",  marginLeft: "100px"}}></img>
+    <p>{doctorDetail.name.first} {doctorDetail.name.last}</p>
+    </Col>
+
+                )
+                   
+   
+             
                 })}
+                  </Row>
+                
             </div>
+                
         );
     }
 }
