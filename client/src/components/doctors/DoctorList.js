93e26d893f79e0fbe03df464f711d881d1c458ca
MarigonaN <marigonanuhija@gmail.com>
2020-11-11 23:38:14 +0100

    Update DoctorList.js

--- client/src/components/doctors/DoctorList.js
+++ client/src/components/doctors/DoctorList.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import DoctorData from './doctors.json'
 import {Row, Col} from 'react-bootstrap'
 class DoctorList extends Component {
+    
+
     render() {
+    
         return (
+            
             <div>
                 <h3>Doctors</h3>
                 <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3">
