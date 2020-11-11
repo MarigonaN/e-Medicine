1bd258a440582b372e74cb1e5a2b2e56aaf73b5b
MarigonaN <marigonanuhija@gmail.com>
2020-11-11 21:44:13 +0100

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -14,9 +14,9 @@ class Footer extends Component {
                 <div class="row align-items-center">
                     <div class="col-lg-4 text-lg-left">Copyright © Marigona Nuhija 2020</div>
                     <div class="col-lg-4 my-3 my-lg-0">
-                        <a class="btn btn-dark btn-social mx-2" href="https://github.com/MarigonaN"><AiFillGithub /></a>
-                        <a class="btn btn-dark btn-social mx-2" href="https://www.linkedin.com/in/marigona-nuhija-02275a173/"><AiFillLinkedin/></a>
-                        <a class="btn btn-dark btn-social mx-2" href="https://mail.google.com/mail/u/0/#inbox"><SiGmail/></a>
+                        <a class="btn btn-dark btn-social mx-2" href="https://github.com/MarigonaN" target="_blank"><AiFillGithub /></a>
+                        <a class="btn btn-dark btn-social mx-2" href="https://www.linkedin.com/in/marigona-nuhija-02275a173/" target="_blank"><AiFillLinkedin/></a>
+                        <a class="btn btn-dark btn-social mx-2" href="https://mail.google.com/mail/u/0/#inbox" target="_blank"><SiGmail/></a>
                     </div>
                     <div class="col-lg-4 text-lg-right">
                         <a class="mr-3" href="/" style={{color: "grey",textDecoration: "none"}}>Privacy Policy</a>
