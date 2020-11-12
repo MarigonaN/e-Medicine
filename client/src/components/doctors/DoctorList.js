da1c41fdf9f524abe76dd81c33622dbad8ebc6bd
MarigonaN <marigonanuhija@gmail.com>
2020-11-13 00:24:14 +0100

    styling

--- client/src/components/doctors/DoctorList.js
+++ client/src/components/doctors/DoctorList.js
@@ -2,41 +2,48 @@ import React, { useState } from 'react';
 import DoctorData from './doctors.json'
 import Modal from 'react-modal'
 import { Row, Col } from 'react-bootstrap'
-function DoctorList()  {
-  
+import { BiPlusMedical } from "react-icons/bi";
+import './DoctorList.css'
+Modal.setAppElement('#root')
+function DoctorList() {
 
-  
-        const  [modalIsOpen,setModalIsOpen] = useState(false)
-        return (
 
-            <div>
-                <h3>Doctors</h3>
-                <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3">
-                    {DoctorData.map((doctorDetail, index) => {
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    return (
 
-                        return (
-                        
-                            <Col key={doctorDetail._id}>
-                              <button onClick={()=> setModalIsOpen(true)}>  <img src={doctorDetail.picture} style={{ width: "250px", height: "250px" }}></img></button>
+        <div>
+            <h3>Doctors</h3>
+            <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3">
+                {DoctorData.map((doctorDetail, index) => {
+
+                    return (
+
+
+
+                        <div class="container">
+                            <Col key={doctorDetail._id} >
+                               <img src={doctorDetail.picture} className="image" style={{ width: "250px", height: "250px" }}></img>
                                 <p >{doctorDetail.name.first} {doctorDetail.name.last}</p>
                             </Col>
-                           
-                         
-                        )
-                      
-                    })}
-                </Row>
-                <Modal isOpen={modalIsOpen}>
-                        <h2>Modal title</h2>
-                        <div>
-                            <button onClick={()=> setModalIsOpen(false)}>Close</button>
+                            <div class="middle">
+                            <BiPlusMedical onClick={() => setModalIsOpen(true)} className="text" size="50px" style={{marginRight: "110px", marginBottom: "30px"}}/>
+                            </div>
                         </div>
-                    </Modal>
+                    )
 
-            </div>
+                })}
+            </Row>
+            <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+                <h2>Modal title</h2>
+                <div>
+                    <button onClick={() => setModalIsOpen(false)}>Close</button>
+                </div>
+            </Modal>
 
-        );
-    
+        </div>
+
+    );
+
 }
 
 export default DoctorList;
